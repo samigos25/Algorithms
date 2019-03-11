6bcea1c7324c718310a90d73f910ada65da8a9b0
samigos25 <samigos25@users.noreply.github.com>
2019-03-11 15:15:17 +0300

    Task 1, 2

---
+++ AlgLesson4/Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+     //  8- -1
+     //7  | | 2
+     //|--  --|
+     //|--  --|
+     //6  | | 3
+     //  5- -4
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ AlgLesson4/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static int[,] arr = new int[,]
+        {
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 0, 1},
+            {1, 1, 0, 1, 1, 1, 1, 1},
+            {1, 1, 0, 1, 1, 1, 1, 1},
+            {0, 1, 0, 1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 1, 1, 0, 1},
+            {0, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        static void Main(string[] args)
+        {
+            printarr(2);
+
+            for (int i = 1; i < 8; i++)
+            {
+                for (int j = 1; j < 8; j++)
+                {
+                    
+                    arr[i, j] = arr[i, j] != 0 ? arr[i - 1, j] + arr[i, j - 1] : 0;
+                }
+            }
+
+            Console.WriteLine();
+            printarr(5);
+
+            Console.ReadKey();
+        }
+
+        static void printarr(int pad)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(arr[i, j].ToString().PadLeft(pad));
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
